f56135c017a4df478c9a8268a6abfa749cd2e6e2
Gloria Friesen and Megan Warnock <megan@>
2017-02-15 15:18:53 -0800

    add change celcius to fahrenheit edited this comment to test ammending a commit

--- js/scripts.js
+++ js/scripts.js
@@ -3,17 +3,23 @@ var liter = function(gallon) {
 	return gallon*(3.785411784);
 };
 
-
-
+//This function converts celcius to fahrenheit
+ var conversion = function(celcius) {
+ 	return ((celcius *=9) /5) +32;
+ 	};
 
 //User interface
 $(document).ready(function() {
 	$("form#gallonLiter").submit(function() {
 		var gallon = parseInt($("input#gallonLiter").val());
-	
 		var result = liter(gallon);
 		$("#outputGallonLiter").text(result);
 		event.preventDefault();
 	});
-
+	$("form#celciusFahrenheit").submit(function(){
+		var celcius = parseInt($("input#celciusFahrenheit").val());
+		var result = conversion(celcius);
+		$("#outputCelciusFahrenheit").text(result);
+		event.preventDefault();
+	});
 });
